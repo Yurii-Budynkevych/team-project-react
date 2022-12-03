820130463f48f0c0a10e8c0f7ca9cd38c5eb5714
ArturMandryk <ArturMandryk@gmail.com>
2022-12-03 12:54:12 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,6 @@
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, Navigate } from 'react-router-dom';
 import { PublicRoute } from '../utils/routes/PublicRoute';
 import { PrivateRoute } from '../utils/routes/PrivateRoute';
 import { Layout } from 'pages/layout/Layout';
@@ -13,11 +13,8 @@ import { refresh } from 'redux/Auth/authOperations';
 import {
   selectIsSaveRoute,
   selectIsRefreshing,
+  selectIsLoggedIn,
 } from '../redux/Auth/authSelectors';
-
-
-import { DiagramTab } from './DiagramTab/DiagramTab';
-
 
 export const App = () => {
   const isSaveRoute = useSelector(selectIsSaveRoute);
@@ -31,44 +28,46 @@ export const App = () => {
 
   return (
     <>
-      
+      {!isRefreshing && (
         <Routes>
           <Route path="/" element={<Layout />}>
-            <Route index element={isLoggedIn? <Navigate to='/home'/>: <Navigate to='/login'/>}/>
-
-              <Route
-                path='home'
-                element={
-                  <PrivateRoute redirectTo="/login" component={<HomePage />} />
-                }
-              />
-              <Route
-                path="statistics"
-                element={
-                  <PrivateRoute
-                    redirectTo="/login"
-                    component={<DiagramTab />}
-                  />
-                }
-              />
-            </Route>
             <Route
-              path="login"
+              index
               element={
-                <PrivateRoute redirectTo="/home" component={<HomePage />} />
+                isLoggedIn ? <Navigate to="/home" /> : <Navigate to="/login" />
+              }
+            />
+
+            <Route
+              path="home"
+              element={
+                <PrivateRoute redirectTo="/login" component={<HomePage />} />
               }
             />
             <Route
-              path="register"
+              path="statistics"
               element={
-                <PublicRoute
-                  redirectTo="/"
-                  component={<RegisterPage />}
-                  restricted
-                />
+                <PrivateRoute redirectTo="/login" component={<DiagramTab />} />
               }
             />
           </Route>
+          <Route
+            path="login"
+            element={
+              <PrivateRoute redirectTo="/home" component={<HomePage />} />
+            }
+          />
+          <Route
+            path="register"
+            element={
+              <PublicRoute
+                redirectTo="/"
+                component={<RegisterPage />}
+                restricted
+              />
+            }
+          />
+
           <Route
             path="login"
             element={
@@ -85,17 +84,16 @@ export const App = () => {
             }
           />
 
-            <Route
-              path="*"
-              element={
-                <PublicRoute>
-                  <ErrorPathPage />
-                </PublicRoute>
-              }
-            />
-
+          <Route
+            path="*"
+            element={
+              <PublicRoute>
+                <ErrorPathPage />
+              </PublicRoute>
+            }
+          />
         </Routes>
-      
+      )}
     </>
   );
 };
