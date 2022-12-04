d55005947610b27d78c91da39a3dd78bec718903
saintajax <saintajax@gmail.com>
2022-12-04 14:45:11 +0200

    header mobile

--- src/components/UserMenu/UserMenu.jsx
+++ src/components/UserMenu/UserMenu.jsx
@@ -3,9 +3,12 @@ import { useDispatch, useSelector } from 'react-redux';
 import { logout } from 'redux/Auth/authOperations';
 import exitSvg from '../../img/exit.svg';
 import { selectUsername } from 'redux/Auth/authSelectors';
-import { Btn, Name } from './UserMenu.styled';
+import { Btn, Name, Img } from './UserMenu.styled';
+import { useMediaQuery } from 'react-responsive';
 
 export const UserMenu = () => {
+  const isTabOrDesk = useMediaQuery({ query: '(min-width: 768px)' });
+
   const dispatch = useDispatch();
   const name = useSelector(selectUsername);
 
@@ -19,8 +22,8 @@ export const UserMenu = () => {
           dispatch(logout());
         }}
       >
-        <img src={exitSvg} alt="exit"></img>
-        Exit
+        <Img src={exitSvg} alt="exit"></Img>
+        {isTabOrDesk && 'Exit'}
       </Btn>
     </Box>
   );
