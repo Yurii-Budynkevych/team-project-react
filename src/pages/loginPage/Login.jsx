8f3495177a7c0f5342026667df81bf08f6a64ea7
saintajax <saintajax@gmail.com>
2022-12-04 12:47:43 +0200

    final

--- src/pages/loginPage/Login.jsx
+++ src/pages/loginPage/Login.jsx
@@ -1,3 +1,11 @@
+import { useMediaQuery } from 'react-responsive';
+import { LoginForm } from '../../components/LoginForm/LoginForm';
+import { Section } from '../../components/Section/Section';
+import { InvitBox } from '../../components/InvitBox/InvitBox';
+import { Elipse1, Elipse2 } from './loginPage.styled';
+import elipseR from '../../img/ellipse2.png';
+import elipseB from '../../img/ellipse1.png';
+
 export default function LoginPage() {
   const isMobile = useMediaQuery({ query: '(max-width: 767px)' });
 
--- src/pages/registerPage/Register.jsx
+++ src/pages/registerPage/Register.jsx
@@ -1,3 +1,11 @@
+import { useMediaQuery } from 'react-responsive';
+import { RegisterForm } from '../../components/RegisterForm/RegisterForm';
+import { Section } from '../../components/Section/Section';
+import { InvitBox } from '../../components/InvitBox/InvitBox';
+import { Elipse1, Elipse2 } from './Register.styled';
+import elipseR from '../../img/ellipse2.png';
+import elipseB from '../../img/ellipse1.png';
+
 export default function RegisterPage() {
   const isMobile = useMediaQuery({ query: '(max-width: 767px)' });
 
